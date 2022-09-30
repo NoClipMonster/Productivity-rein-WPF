80400081c9c1f504e8cece2aca028df9cd3abc27
NoClipMonster <iavedis9@gmail.com>
2022-09-30 21:30:17 +0300

    Creat Files

---
+++ MyControls/ListOfTypes.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Productivity_rein_WPF.MyControls {
+    /// <summary>
+    /// Interaction logic for ListOfTypes.xaml
+    /// </summary>
+    public partial class ListOfTypes : UserControl {
+        public ListOfTypes() {
+            InitializeComponent();
+        }
+    }
+}
